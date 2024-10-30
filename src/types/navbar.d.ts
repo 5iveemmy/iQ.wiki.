591c5b7b3540b639b4334e6911868b985d9ac75f
5iveemmy <ainemmanuel87@gmail.com>
2024-10-30 17:39:34 +0100

    chore: add global type for navbar component

---
+++ src/types/navbar.d.ts
@@ -0,0 +1,20 @@
+export {};
+
+declare global {
+  interface MenuItems {
+    title: string;
+    icon: React.JSX.Element;
+    href: string;
+  }
+
+  interface Languages {
+    language: string;
+    abbr: string;
+    flag: string;
+  }
+
+  interface MenuLinks {
+    href: string;
+    title: string;
+  }
+}
